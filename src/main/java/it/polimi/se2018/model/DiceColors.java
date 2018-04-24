ffc7a2215c2a240672c296ebef907c6b61c14104
Federico Haag <federico.haag@mail.polimi.it>
2018-04-24 23:34:09 +0200

    Add DraftPool Class
    Minor add: getRandomColor in DiceColors Enum

--- src/main/java/it/polimi/se2018/model/DiceColors.java
+++ src/main/java/it/polimi/se2018/model/DiceColors.java
@@ -1,10 +1,17 @@
 package it.polimi.se2018.model;
 
+import java.util.Random;
+
 public enum DiceColors {
     NOCOLOR,
     RED,
     YELLOW,
     GREEN,
     BLUE,
-    PURPLE
+    PURPLE;
+
+    public static DiceColors getRandomColor(){
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
 }
---
+++ src/main/java/it/polimi/se2018/model/DraftPool.java
@@ -0,0 +1,38 @@
+package it.polimi.se2018.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DraftPool {
+
+    //List of dices of the draftpool
+    public List<Dice> dices;
+
+    public DraftPool() {
+        this.dices = new ArrayList<>();
+    }
+
+    public DraftPool(List<Dice> dices){
+        this.dices = dices;
+    }
+
+    //Rolls each dice in draftpool
+    public void reroll() {
+
+        for (Dice dice : dices) {
+            dice.roll();
+        }
+    }
+
+    //Removes from the draftpool the specified dice
+    public boolean takeDice(Dice dice) {
+
+        return dices.remove(dice);
+    }
+
+    //Add the specified dice to the draftpool
+    public void addDice(Dice dice) {
+
+        dices.add(dice);
+    }
+}
