ec7ff9b614b73f0cf09c33d05bf4400c61716421
jacopogargano <jacopopio.gargano@mail.polimi.it>
2018-04-29 02:24:00 +0200

    Add PublicObjectiveCard Abstract Class

---
+++ src/main/java/it/polimi/se2018/model/PublicObjectiveCard.java
@@ -0,0 +1,7 @@
+package it.polimi.se2018.model;
+
+//Abstract class that extends ObjectiveCard with no additional feature
+//The objective of this abstract class is to better understand that a Game has a certain amount of public objective cards
+//which are visible to every player, whereas the private ones are only visible to the player who owns them.
+
+public abstract class PublicObjectiveCard extends ObjectiveCard{}
