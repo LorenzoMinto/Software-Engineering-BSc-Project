5dd3094bc146e95c6e6c94a68e6e8af997abec4a
LorenzoMinto <Sebalollo96>
2018-05-01 14:24:33 +0200

    Add early Round class
    This is an early, streamlined version of Round in wait of the definition of Turn.

---
+++ src/main/java/it/polimi/se2018/model/Round.java
@@ -0,0 +1,28 @@
+package it.polimi.se2018.model;
+
+import java.util.List;
+
+public class Round {
+
+    private int number;
+    private Player[] players;
+    private DraftPool draftPool;
+    private DiceBag diceBag;
+
+    public Round(int number, Player[] players, DiceBag diceBag) {
+        this.number = number;
+        this.players = players;
+        this.diceBag = diceBag;
+        this.draftPool = diceBag.getDices(players.length*2+1);
+    }
+
+    public List<Dice> getDraftPool(){
+        return draftPool.dices;
+    }
+
+    public boolean removeDiceFromDraftPool(Dice dice) {
+        return draftPool.takeDice(dice);
+    }
+
+
+}
