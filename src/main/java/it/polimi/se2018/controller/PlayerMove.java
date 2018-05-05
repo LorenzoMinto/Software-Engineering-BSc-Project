eecaf63bc8044805c0312a73d216b7547f45c24d
Federico Haag <federico.haag@mail.polimi.it>
2018-05-05 19:45:06 +0200

    Add PlayerMove class
    +Add UseToolCardPlayerMove class

--- src/main/java/it/polimi/se2018/controller/PlayerMove.java
+++ src/main/java/it/polimi/se2018/controller/PlayerMove.java
@@ -1,18 +1,21 @@
 package it.polimi.se2018.controller;
 
 import it.polimi.se2018.model.Player;
+import it.polimi.se2018.view.View;
 
 
 public abstract class PlayerMove {
 
+    //TODO: check access permissions to the following attributes
     Player player;
 
     View view;
-
-    public abstract void perform(ControllerState controllerState);
 
     public Player getPlayer() {
         return player;
     }
 
+    public View getView() {
+        return view;
+    }
 }
---
+++ src/main/java/it/polimi/se2018/controller/UseToolCardPlayerMove.java
@@ -0,0 +1,12 @@
+package it.polimi.se2018.controller;
+
+import it.polimi.se2018.model.ToolCard;
+
+public class UseToolCardPlayerMove extends PlayerMove {
+
+    private ToolCard toolcard;
+
+    public ToolCard getToolcard() {
+        return toolcard;
+    }
+}
