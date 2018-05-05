9243466250f3cd1acf883df0773cd8e68a832bcc
Federico Haag <federico.haag@mail.polimi.it>
2018-05-05 17:29:04 +0200

    Add empty() method to DraftPool

--- src/main/java/it/polimi/se2018/model/DraftPool.java
+++ src/main/java/it/polimi/se2018/model/DraftPool.java
@@ -30,14 +30,20 @@ public class DraftPool {
     }
 
     //Removes from the draftpool the specified dice
-    public boolean takeDice(Dice dice) {
+    public boolean draftDice(Dice dice) {
 
         return dices.remove(dice);
     }
 
     //Add the specified dice to the draftpool
-    public void addDice(Dice dice) {
+    public void putDice(Dice dice) {
 
         dices.add(dice);
+    }
+
+    public List<Dice> empty(){
+        List<Dice> d = new ArrayList<>(this.dices);
+        this.dices = new ArrayList<>();
+        return d;
     }
 }
