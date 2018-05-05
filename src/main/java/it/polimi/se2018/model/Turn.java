37517adfb0d78149a0e6798d4292a76d80d1cc2f
Federico Haag <federico.haag@mail.polimi.it>
2018-05-05 19:39:53 +0200

    Add Turn class

---
+++ src/main/java/it/polimi/se2018/model/Turn.java
@@ -0,0 +1,71 @@
+package it.polimi.se2018.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Turn {
+    public Turn(int number, Player player) {
+        this.number = number;
+        this.currentPlayer = player;
+        this.draftedDice = null;
+        this.draftedAndPlaced = false;
+        this.usedToolCard = null;
+    }
+
+    private int number;
+
+    public Player currentPlayer;
+
+    private boolean draftedAndPlaced;
+
+    private Dice draftedDice;
+
+    private ToolCard usedToolCard;
+
+
+    //Getters
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean hasDrafted(){
+        return !( draftedDice == null );
+    }
+
+    public boolean hasDraftedAndPlaced(){
+        return draftedAndPlaced;
+    }
+
+    public boolean hasUsedToolCard(){
+        return !( this.usedToolCard == null );
+    }
+
+    //Setters
+
+    public void setDraftedDice(Dice dice){
+        this.draftedDice = dice;
+    }
+
+    public void setDraftedAndPlaced(){
+        if(this.draftedDice==null){ throw new IllegalStateException("Asked to setDraftedAndPlaced but draftedDice is null"); }
+        this.draftedAndPlaced = true;
+    }
+
+    public void setUsedToolCard(ToolCard card){
+        this.usedToolCard = card;
+    }
+
+    public void setCurrentDiceAsPlaced(){
+        this.draftedAndPlaced = true;
+    }
+
+    //Utils
+
+    public boolean isCurrentPlayer(Player player){
+        return ( this.currentPlayer.equals(player) );
+    }
+
+
+
+}
