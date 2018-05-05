0ec6259de44970975aecff8e3abf75af8922553b
jacopogargano <jacopopio.gargano@mail.polimi.it>
2018-05-06 01:09:58 +0200

    Add PlayerMove classes

--- src/main/java/it/polimi/se2018/controller/UseToolCardPlayerMove.java
+++ src/main/java/it/polimi/se2018/controller/UseToolCardPlayerMove.java
@@ -6,6 +6,10 @@ public class UseToolCardPlayerMove extends PlayerMove {
 
     private ToolCard toolcard;
 
+    public UseToolCardPlayerMove(ToolCard toolcard) {
+        this.toolcard = toolcard;
+    }
+
     public ToolCard getToolcard() {
         return toolcard;
     }
