facc9d220707cbe4407a4be52e7ff30fc15fb3a3
Lorenzo Minto <lorenzomnto@gmail.com>
2018-05-06 10:22:35 +0200

    Update StartControllerState and implement methods

--- src/main/java/it/polimi/se2018/controller/StartControllerState.java
+++ src/main/java/it/polimi/se2018/controller/StartControllerState.java
@@ -1,8 +1,7 @@
 package it.polimi.se2018.controller;
 
-import it.polimi.se2018.model.Dice;
-import it.polimi.se2018.model.Player;
-import it.polimi.se2018.model.ToolCard;
+import it.polimi.se2018.model.*;
+import it.polimi.se2018.view.View;
 
 public class StartControllerState implements ControllerState {
     Controller controller;
@@ -12,18 +11,23 @@ public class StartControllerState implements ControllerState {
     }
 
     @Override
-    public void draftDiceFromDraftPool(Dice dice) {
-        //work magic on model here
+    public void draftDiceFromDraftPool(Dice dice, View view) {
+        Game game = controller.game;
+        Round currentRound = game.currentRound;
+        if (currentRound.draftPool.draftDice(dice)) {
+            currentRound.currentTurn.setDraftedDice(dice);
+            controller.hasDrafted = true;
+        }
         controller.setControllerState(controller.getPlaceState());
     }
 
     @Override
-    public void placeDice(int row, int col) {
-        System.out.println("Can't do. You have to choose a dice from the draft pool first.");
+    public void placeDice(int row, int col, View view) {
+        view.showMessage("Can't do. You have to choose a dice from the draft pool first.");
     }
 
     @Override
-    public boolean useToolCard(Player player, ToolCard toolcard) {
+    public boolean useToolCard(Player player, ToolCard toolcard, View view) {
         if (controller.canUseSpecificToolCard(player, toolcard)) {
             controller.setActiveToolCard(toolcard);
             controller.setControllerState(toolcard.nextState(this));
@@ -33,32 +37,32 @@ public class StartControllerState implements ControllerState {
     }
 
     @Override
-    public void draftDiceFromTrack(Dice dice) {
-        System.out.println("Can't do. Choose a dice from the draft pool or activate a ToolCard");
+    public void draftDiceFromTrack(Dice dice, View view) {
+        view.showMessage("Can't do. Choose a dice from the draft pool or activate a ToolCard");
     }
 
     @Override
-    public void moveDice(int rowFrom, int colFrom, int rowTo, int colTo) {
-        System.out.println("Can't do. Choose a dice from the draft pool or activate a ToolCard");
+    public void moveDice(int rowFrom, int colFrom, int rowTo, int colTo, View view) {
+        view.showMessage("Can't do. Choose a dice from the draft pool or activate a ToolCard");
     }
 
     @Override
-    public void incrementDice() {
-        System.out.println("Can't do. Choose a dice from the draft pool or activate a ToolCard");
+    public void incrementDice(View view) {
+        view.showMessage("Can't do. Choose a dice from the draft pool or activate a ToolCard");
     }
 
     @Override
-    public void decrementDice() {
-        System.out.println("Can't do. Choose a dice from the draft pool or activate a ToolCard");
+    public void decrementDice(View view) {
+        view.showMessage("Can't do. Choose a dice from the draft pool or activate a ToolCard");
     }
 
     @Override
-    public void chooseDiceValue(int value) {
-        System.out.println("Can't do. Choose a dice from the draft pool or activate a ToolCard");
+    public void chooseDiceValue(int value, View view) {
+        view.showMessage("Can't do. Choose a dice from the draft pool or activate a ToolCard");
     }
 
     @Override
-    public void executeImplicitBehaviour() {
+    public void executeImplicitBehaviour(View view) {
         //TODO: initialize commented variables below only when at beginning of turn
 //        controller.hasDrafted = false;
 //        controller.hasToolCardBeenUsed = false;
