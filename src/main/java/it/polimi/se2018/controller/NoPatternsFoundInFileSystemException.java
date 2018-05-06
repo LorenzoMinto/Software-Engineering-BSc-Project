3a1fd457ee330976e682f9dbb0013bb72cfe7337
Federico Haag <federico.haag@mail.polimi.it>
2018-05-06 20:03:59 +0200

    Moved factories to Controller
    +Moved Exceptions throwed by moved factories

--- src/main/java/it/polimi/se2018/controller/NoPatternsFoundInFileSystemException.java
+++ src/main/java/it/polimi/se2018/controller/NoPatternsFoundInFileSystemException.java
@@ -1,4 +1,4 @@
-package it.polimi.se2018.model;
+package it.polimi.se2018.controller;
 
 public class NoPatternsFoundInFileSystemException extends Exception {
     public NoPatternsFoundInFileSystemException() {
