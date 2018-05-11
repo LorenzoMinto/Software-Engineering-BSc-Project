e733768bf25de2883dd53e70ab480d2c0def8cf7
Federico Haag <federico.haag@mail.polimi.it>
2018-05-11 18:59:05 +0200

    Fix commit of App.java

--- src/main/java/it/polimi/se2018/App.java
+++ src/main/java/it/polimi/se2018/App.java
@@ -17,7 +17,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
+        /*
         //TODO: load from config file
         int numberOfRounds = 10;
         int maxNumberOfPlayers = 4;
@@ -42,6 +42,7 @@ public class App
 
             return; //here just to prevent infinite loop. it is not logically-correct. delete after implementing correctly while(true)
         }
+        */
 
     }
 }
