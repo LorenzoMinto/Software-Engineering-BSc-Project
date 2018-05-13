79bddfaaa88dae079cef1597b896f03bcd3bb845
Federico Haag <federico.haag@mail.polimi.it>
2018-05-13 13:55:01 +0200

    Add JavaDoc of Observable and Observer

--- src/main/java/it/polimi/se2018/utils/Observer.java
+++ src/main/java/it/polimi/se2018/utils/Observer.java
@@ -1,5 +1,21 @@
 package it.polimi.se2018.utils;
 
-public interface Observer<T> {
-    void update(T message);
+
+import it.polimi.se2018.model.ChangeMessage;
+
+/**
+ * A class can implement the {@link Observer} interface when it
+ * wants to be informed of changes in observable objects.
+ */
+public interface Observer {
+
+    /**
+     * This method is called whenever the observed object is changed. An
+     * application calls an {@link Observable} object's
+     * {@link Observable#notify(ChangeMessage)} method to have all the object's
+     * observers notified of the change.
+     *
+     * @param m an argument passed to the {@link Observable#notify(ChangeMessage)} method
+     */
+    void update(ChangeMessage m);
 }
