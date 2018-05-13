8bb48f44d5efc56c1f60d574a2237b2b5fcf7b2b
Lorenzo Minto <lorenzomnto@gmail.com>
2018-05-13 23:24:24 +0200

    Add SkipNextTurnControllerState class

---
+++ src/main/java/it/polimi/se2018/controller/SkipNextTurnControllerState.java
@@ -0,0 +1,29 @@
+package it.polimi.se2018.controller;
+
+import it.polimi.se2018.model.Round;
+
+/**
+ * @author Lorenzo Minto
+ */
+public class SkipNextTurnControllerState extends ImplicitControllerState {
+
+    /**
+     * Class constructor.
+     *
+     * @param controller the controller of which this class is going to act as a state.
+     */
+    public SkipNextTurnControllerState(Controller controller) {
+        super(controller);
+    }
+
+    /**
+     * Eliminates the current player's next turn from current round.
+     */
+    @Override
+    public void executeImplicitBehaviour() {
+        Round currentRound = controller.game.getCurrentRound();
+        if (currentRound.removeNextTurnOfPlayer(currentRound.getCurrentTurn().getPlayer())) {
+            controller.stateManager.getNextState(this);
+        }
+    }
+}
