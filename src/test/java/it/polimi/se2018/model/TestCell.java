1ab5ba746b6b354f0153f0c467f9483bf055892f
Federico Haag <federico.haag@mail.polimi.it>
2018-05-13 22:34:33 +0200

    Add Tests for DiceBag, Dice, Cell
    +Fix bug in DiceBag and DiceColors

---
+++ src/test/java/it/polimi/se2018/model/TestCell.java
@@ -0,0 +1,39 @@
+package it.polimi.se2018.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestCell {
+
+    @Test
+    public void testGetAllowedValue() {
+        Cell cell = new Cell(3,DiceColors.PURPLE);
+        assertEquals(cell.getAllowedValue(),3);
+    }
+
+    @Test
+    public void testGetAllowedColor() {
+        Cell cell = new Cell(3,DiceColors.PURPLE);
+        assertEquals(DiceColors.PURPLE,cell.getAllowedColor());
+    }
+
+    @Test
+    public void testRemoveDice() {
+        Cell cell = new Cell(3,DiceColors.PURPLE);
+        Dice dice = new Dice(DiceColors.GREEN,4);
+        cell.setDice(dice);
+        Dice diceRemoved = cell.removeDice();
+        assertTrue(diceRemoved.equals(dice));
+    }
+
+    @Test
+    public void testToString() {
+        Cell cell = new Cell(3,DiceColors.PURPLE);
+        Dice dice = new Dice(DiceColors.GREEN,4);
+        cell.setDice(dice);
+        assertEquals("(4:G)",cell.toString());
+        cell.removeDice();
+        assertEquals("(3:P)",cell.toString());
+    }
+}
