2a10c34570f79533ea729d0a7d58f062608021c3
Lorenzo Minto <lorenzomnto@gmail.com>
2018-05-13 02:50:50 +0200

    Add Java Doc to Track and TrackSlot

--- src/main/java/it/polimi/se2018/model/Track.java
+++ src/main/java/it/polimi/se2018/model/Track.java
@@ -2,22 +2,49 @@ package it.polimi.se2018.model;
 
 import java.util.List;
 
+
+/**
+ * Class representing the Game's Track, where the leftover dices from the {@link DraftPool} are kept at the end of
+ * every {@link Round}. The dices are stacked if more than one.
+ */
 public class Track {
 
+
+    /**
+     * List of {@link TrackSlot}, on which the leftover dices of the draft pool of every round are kept.
+     */
     private List<TrackSlot> slots;
 
-    //Creates new TrackSlot with list of dices passed
+
+    /**
+     * Creates new TrackSlot with the list of dice passed and appends it to the list.
+     *
+     * @param dices list of dices passed.
+     */
     public void processDices(List<Dice> dices) {
         TrackSlot slot = new TrackSlot(dices);
         slots.add(slot);
     }
 
+
+    /**
+     * Return a list of dices kept in the TrackSlot with given slot number.
+     *
+     * @param slotNumber the number of the wanted TrackSlot.
+     * @return the list of dices kept in the wanted TrackSlot.
+     */
     public List<Dice> getDicesFromSlotNumber(int slotNumber) {
         TrackSlot slot = slots.get(slotNumber);
         return slot.getDices();
     }
 
-    //Removes dice from corresponding TrackSlot
+    /**
+     * Removes the specified dice kept on the TrackSlot with specified slot number.
+     *
+     * @param dice the dice to be removed.
+     * @param slotNumber the TrackSlot number from which to remove the dice.
+     * @return whether or not the specified dice could be removed from the specified TrackSlot.
+     */
     public boolean takeDice(Dice dice, int slotNumber){
         if (slotNumber >= slots.size()) {
             //TrackSlotIndexOutOfBound
@@ -27,7 +54,13 @@ public class Track {
         return slot.removeDice(dice);
     }
 
-    //Returns false if slotNumber indexed is not found, else adds dice to corresponding TrackSlot
+    /**
+     * Puts given dice in the TrackSlot with specified slot number.
+     *
+     * @param dice the dice to be put.
+     * @param slotNumber the TrackSlot number on which to put the dice.
+     * @return whether or not the specified dice could be put on the specified TrackSlot.
+     */
     public boolean putDice(Dice dice, int slotNumber){
         if (slotNumber >= slots.size()) {
             return false;
--- src/main/java/it/polimi/se2018/model/TrackSlot.java
+++ src/main/java/it/polimi/se2018/model/TrackSlot.java
@@ -3,21 +3,49 @@ package it.polimi.se2018.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class representing the single slots of the Game's Track (one for each round).
+ */
 public class TrackSlot {
 
+    /**
+     * List of dices kept in the TrackSlot
+     */
     private List<Dice> dices;
 
+
+    /**
+     * Class constructor.
+     *
+     * @param dices the list of dices with which to initialize the TrackSlot.
+     */
     public TrackSlot(List<Dice> dices) { this.dices = dices; }
 
+
+    /**
+     * Adds a given dice to the TrackSlot's list of dices.
+     *
+     * @param dice the dice to be added.
+     */
     public void addDice(Dice dice) {
         dices.add(dice);
     }
 
+    /**
+     * Removes, if possible, a specified dice from the TrackSlot's dices.
+     *
+     * @param dice the dice to be removed.
+     * @return whether or not specified dice could be removed from the TrackSlot's dices.
+     */
     public boolean removeDice(Dice dice) {
-        if (dices.contains(dice)) { return dices.remove(dice); }
-        return false;
+        return dices.contains(dice) && dices.remove(dice);
     }
 
+    /**
+     * Returns a copy of the list of dices in the TrackSlot.
+     *
+     * @return a copy of the list of dices.
+     */
     public List<Dice> getDices() {
         return new ArrayList<>(dices);
     }
