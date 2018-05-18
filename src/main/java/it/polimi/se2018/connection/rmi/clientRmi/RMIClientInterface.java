c5e4fc4b300f92967c7c14e6522f294ec0979c7a
jacopogargano <jacopopio.gargano@mail.polimi.it>
2018-05-18 23:36:39 +0200

    Remove Connection package (substitute with Networking package)

--- src/main/java/it/polimi/se2018/connection/rmi/clientRmi/RMIClientInterface.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.se2018.connection.rmi.clientRmi;
-
-import it.polimi.se2018.connection.ClientImplementationInterface;
-
-import java.rmi.Remote;
-
-public interface RMIClientInterface extends Remote, ClientImplementationInterface {
-
-}
