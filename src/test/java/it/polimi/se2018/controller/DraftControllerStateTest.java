162997d60732b56ade951cdf1927c774fd79e4a3
Lorenzo Minto <lorenzomnto@gmail.com>
2018-05-29 02:02:32 +0200

    Add DraftControllerState unit test

---
+++ src/test/java/it/polimi/se2018/controller/DraftControllerStateTest.java
@@ -0,0 +1,85 @@
+package it.polimi.se2018.controller;
+
+import it.polimi.se2018.model.*;
+import it.polimi.se2018.utils.message.Message;
+import it.polimi.se2018.utils.message.VCMessage;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static it.polimi.se2018.utils.message.CVMessage.types.ERROR_MESSAGE;
+import static org.junit.Assert.*;
+
+public class DraftControllerStateTest {
+    private Controller controller;
+    private Properties prop;
+
+    @Before
+    public void setUp() throws Exception {
+        Game game = new Game(4,4);
+        Properties gprop = new Properties();
+        gprop.setProperty("numberOfRounds","10");
+        gprop.setProperty("numberOfDicesPerColor","18");
+        gprop.setProperty("numberOfToolCards","3");
+        gprop.setProperty("numberOfPublicObjectiveCards","2");
+        gprop.setProperty("maxNumberOfPlayers","4");
+        gprop.setProperty("minNumberOfPlayers","2");
+        gprop.setProperty("timeoutLaunchingGame","30");
+        gprop.setProperty("timeoutChoosingPatterns","30");
+        gprop.setProperty("amountOfCouplesOfPatternsPerPlayer","4");
+
+        controller = new Controller(game, gprop);
+
+        Set<String> nicknames = new HashSet<>(Arrays.asList("johnnifer", "rubens"));
+
+        WindowPatternManager wpmanager = new WindowPatternManager();
+        WindowPattern wp = wpmanager.getCouplesOfPatterns(1).iterator().next();
+
+        controller.launchGame(nicknames);
+
+        for (Player p : controller.game.getPlayers()) {
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("windowPattern", wp);
+            controller.handleMove(new VCMessage(VCMessage.types.CHOOSE_WINDOW_PATTERN, params, p.getID()));
+        }
+
+        prop = new Properties();
+        prop.put("id", "id");
+        prop.put("title", "title");
+        prop.put("description", "desc");
+        prop.put("neededTokens", "1");
+        prop.put("tokensUsageMultiplier", "2");
+        prop.put("imageURL", "imageURL");
+
+        controller.controllerState = controller.stateManager.getDraftControllerState();
+    }
+
+    @Test
+    public void testDraftDiceFromDraftPool() {
+        Dice dice = controller.game.getCurrentRound().getDraftPool().getDices().get(0);
+        controller.controllerState.draftDiceFromDraftPool(dice);
+
+        Round currentRound = controller.game.getCurrentRound();
+        Turn currentTurn = currentRound.getCurrentTurn();
+        assertEquals(dice, currentTurn.getDraftedDice());
+    }
+
+    @Test
+    public void testDraftDiceFromDraftPoolWhenDiceNotInDraftPool() {
+        Dice dice = new Dice(DiceColors.BLUE);
+        while (controller.game.getCurrentRound().getDraftPool().getDices().contains(dice)) {
+            dice = new Dice(DiceColors.getRandomColor());
+        }
+        Message m = controller.controllerState.draftDiceFromDraftPool(dice);
+
+        Turn currentTurn = controller.game.getCurrentRound().getCurrentTurn();
+        assertNull(currentTurn.getDraftedDice());
+    }
+
+    @Test
+    public void testPlaceDice() {
+        Message m = controller.controllerState.placeDice(0,0);
+        assertEquals(ERROR_MESSAGE,m.getType());
+    }
+}
