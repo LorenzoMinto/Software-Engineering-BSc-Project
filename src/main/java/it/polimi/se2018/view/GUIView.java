8b5b5e613c744645258a8ef754b16a70f1d61302
Federico Haag <federico.haag@mail.polimi.it>
2018-05-30 09:34:49 +0200

    Add logger to View

--- src/main/java/it/polimi/se2018/view/GUIView.java
+++ src/main/java/it/polimi/se2018/view/GUIView.java
@@ -3,12 +3,13 @@ package it.polimi.se2018.view;
 import it.polimi.se2018.utils.message.Message;
 import it.polimi.se2018.utils.Observer;
 
+import java.util.logging.Logger;
+
 public class GUIView extends View implements Observer {
 
-    public GUIView() {
-        super();
+    public GUIView(Logger logger) {
+        super(logger);
     }
-
 
     @Override
     public boolean update(Message m) {
