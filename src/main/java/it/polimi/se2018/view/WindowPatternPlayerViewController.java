d7e235fc557194787c4b67a27ef2c097c86fccad
Lorenzo Minto <lorenzomnto@gmail.com>
2018-06-01 19:51:40 +0200

    Add WindowPatternPlayerView fxml and controller

---
+++ src/main/java/it/polimi/se2018/view/WindowPatternPlayerViewController.java
@@ -0,0 +1,27 @@
+package it.polimi.se2018.view;
+
+import it.polimi.se2018.model.Cell;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+public class WindowPatternPlayerViewController {
+    private String nickname;
+    private int favourTokens;
+
+    @FXML private Label nicknameLabel;
+    @FXML private Label favourTokensLabel;
+
+    public WindowPatternPlayerViewController(String nickname, int favourTokens, Cell[][] pattern) {
+        this.nickname = nickname;
+        this.favourTokens = favourTokens;
+    }
+
+    @FXML
+    public void initialize() {
+        nicknameLabel.setText(nickname);
+        favourTokensLabel.setText(String.valueOf(favourTokens));
+    }
+
+}
