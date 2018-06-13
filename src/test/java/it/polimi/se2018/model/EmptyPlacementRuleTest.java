fcc6b0837abe16058328ee8aeeb6f9203126d56f
jacopogargano <jacopopio.gargano@mail.polimi.it>
2018-06-13 15:05:35 +0200

    Update tests + JavaDoc for EmptyPlacementRuleDecorator class

--- src/test/java/it/polimi/se2018/model/EmptyPlacementRuleTest.java
+++ src/test/java/it/polimi/se2018/model/EmptyPlacementRuleTest.java
@@ -1,27 +1,38 @@
 package it.polimi.se2018.model;
 
-import it.polimi.se2018.controller.ObjectiveCardManager;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Test for {@link EmptyPlacementRule} class
+ *
+ * @author Lorenzo Minto
+ */
 public class EmptyPlacementRuleTest {
 
     private static Cell[][] pattern;
 
-    private PlacementRule rule;
-    private PlacementRule decoratedRule;
+    private static PlacementRule rule;
 
     private WindowPattern windowPattern;
 
-    private Dice redDice;
-    private Dice blueDice;
+    private static Dice redDice;
+    private static Dice blueDice;
 
+    private static Player player;
+
+    /**
+     * Initializes the variables needed in the tests
+     */
     @BeforeClass
-    public static void setUpClass() throws Exception {
+    public static void initializeVariables(){
+        rule = new EmptyPlacementRule();
+
         pattern = new Cell[3][3];
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
@@ -29,29 +40,38 @@ public class EmptyPlacementRuleTest {
             }
         }
         pattern[1][1] = new Cell(0, DiceColor.RED);
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        rule = new EmptyPlacementRule();
-        ObjectiveCardManager objectiveCardManager = new ObjectiveCardManager();
-        Player dummy = new Player("Sonny", objectiveCardManager.getPrivateObjectiveCard());
-
-        windowPattern = new WindowPattern("","", "",0, pattern);
-        dummy.setWindowPattern(windowPattern);
-        windowPattern.setOwner(dummy);
 
         redDice = new Dice(DiceColor.RED);
         blueDice = new Dice(DiceColor.BLUE);
+
+        player = new Player("Sonny", PrivateObjectiveCard.createTestInstance());
     }
 
+    /**
+     * Initializes the player's windowPattern for the tests before each test
+     */
+    @Before
+    public void initializeWindowPattern(){
+        windowPattern = new WindowPattern("","", "",0, pattern);
+        player.setWindowPattern(windowPattern);
+    }
+
+    /**
+     * Tests that it is allowed to move a dice on a cell with no dice
+     * @see EmptyPlacementRule#isMoveAllowed(WindowPattern, Dice, int, int)
+     */
     @Test
     public void testCheckIfMoveIsAllowed() {
+        assertNull(windowPattern.getDiceOnCell(1,1));
         assertTrue(rule.isMoveAllowed(windowPattern, redDice, 1, 1));
     }
 
+    /**
+     * Tests the impossibility of moving a dice on a cell that already has a dice
+     * @see EmptyPlacementRule#isMoveAllowed(WindowPattern, Dice, int, int)
+     */
     @Test
-    public void testCheckIfMoveIsAllowedWhenNotAllowed() {
+    public void testCheckIfMoveIsAllowedWhenThereIsAlreadyADiceOnCell() {
         windowPattern.putDiceOnCell(redDice, 1,1);
         assertFalse(rule.isMoveAllowed(windowPattern, blueDice, 1, 1));
     }
