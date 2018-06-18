95de5e369183e4b480c3c940900fb7510db0f3ea
Federico Haag <federico.haag@mail.polimi.it>
2018-06-18 09:53:57 +0200

    Fix javadoc in Player

--- src/main/java/it/polimi/se2018/model/Player.java
+++ src/main/java/it/polimi/se2018/model/Player.java
@@ -14,15 +14,15 @@ public class Player implements Serializable {
      */
     private static final long serialVersionUID = -3361603123980854887L;
     /**
-     * String passed as message of IllegalArgumentException when is asked to
+     * String passed as message of IllegalArgumentException when is asked to decrease tokens of a negative quantity
      */
     private static final String CANNOT_DECREASE_TOKENS_OF_A_NEGATIVE_QUANTITY = "Cannot decrease tokens of a negative quantity";
     /**
-     * String passed as message of IllegalArgumentException when is asked to
+     * String passed as message of IllegalArgumentException when is asked to set a null windowpattern to player
      */
     private static final String SET_A_NULL_WINDOWPATTERN_TO_PLAYER = "Can't set a null windowPattern to player";
     /**
-     * String passed as message of IllegalArgumentException when is asked to
+     * String passed as message of IllegalArgumentException when is asked to create a player giving a null card
      */
     private static final String CREATE_PLAYER_NULL_CARD = "Asked to create a player giving null card";
     /**
