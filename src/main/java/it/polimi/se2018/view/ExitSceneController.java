d30ed4c611c332510b565dda30bd825d7b377179
Lorenzo Minto <lorenzomnto@gmail.com>
2018-06-29 12:23:24 +0200

    Add ExitScene for when the game is quit or aborted

---
+++ src/main/java/it/polimi/se2018/view/ExitSceneController.java
@@ -0,0 +1,28 @@
+package it.polimi.se2018.view;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ * Controller class for ExitScene fxml scene. Displays exit screen after the game has been quit.
+ *
+ * @author Lorenzo Minto
+ */
+public class ExitSceneController {
+
+    @FXML private Label messageLabel;
+
+
+    @FXML public void handleWindowClosing() {
+        Stage stage = (Stage) messageLabel.getScene().getWindow();
+        stage.close();
+    }
+
+    public void setMessage(String message) {
+        this.messageLabel.setText(message);
+    }
+
+}
